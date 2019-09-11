2cc9847b4f72ce4ae3ef04185eae5bc5b7602cfc
unknown <abhishekkumar9608@gmail.com>
2019-09-11 09:26:38 +0530

    API

---
+++ routes/tourRoutes.js
@@ -0,0 +1,28 @@
+  
+const express = require('express');
+const tourController = require('./../controllers/tourController');
+
+
+const router = express.Router();
+
+
+router.param('id', tourController.checkID);
+
+// create a checkBody middleware
+// check if body contains the name and price property 
+// if not, send back 400 (bad reqest)
+// Add it to the post handler stack
+
+
+router
+.route('/')
+.get(tourController.getAllTours)
+.post(tourController.checkBody, tourController.createTour);
+
+router
+.route('/:id')
+.get(tourController.getTour)
+.patch(tourController.updateTour)
+.delete(tourController.deleteTour)
+
+module.exports = router;
