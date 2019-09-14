6fb9deeb99132d91c25efd5d204b23143e9a3cef
unknown <abhishekkumar9608@gmail.com>
2019-09-14 21:45:39 +0530

    mongodbconnect

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
+const Tour = require('./../models/tourModel');
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+
+// const tours = JSON.parse(
+//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+// );
 
 exports.checkID = (req, res, next, val) => {
   console.log(`Tour id is : ${val}`);
@@ -31,10 +33,10 @@ exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours
-    }
+    // results: tours.length,
+    // data: {
+    //   tours
+    // }
   });
 }
 
@@ -42,52 +44,38 @@ exports.getTour = (req, res) => {
   console.log(req.params);
   const id = req.params.id * 1;
 
-  const tour = tours.find(el => el.id === id);
+  // const tour = tours.find(el => el.id === id);
 
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour
-    }
-  })
-}
+  // res.status(200).json({
+  //   status: 'success',
+  //   data: {
+  //     tour
+  //   }
+  // })
+};
 
 exports.createTour = (req, res) => {
-  // console.log(req.body);
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname} /dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    err => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour
-        }
-      });
-    }
-  );
-}
-
-exports.updateTour = (req, res) => {
-
-
-  res.status(200).json({
+  res.status(201).json({
     status: 'success',
-    data: {
-      tour: '<updated tour here...>'
-    }
-  });
-};
+    // data: {
+    //   tour: newTour
+    // }
+  };
+
+  exports.updateTour = (req, res) => {
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour: '<updated tour here...>'
+      }
+    });
+  };
 
 
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
-};
+  exports.deleteTour = (req, res) => {
+    res.status(204).json({
+      status: 'success',
+      data: null
+    });
+  };
