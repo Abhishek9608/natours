f960f98e2f3887d09a5bdb6b02650a9c0ea165c2
unknown <abhishekkumar9608@gmail.com>
2019-09-16 22:40:19 +0530

    refactoringAPI

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,26 +1,22 @@
 const Tour = require('./../models/tourModel');
+const APIFeatures = require('./../utiles/apiFeatures');
 
-// const tours = JSON.parse(
-//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-// );
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
 
 exports.getAllTours = async (req, res) => {
   try {
-    // BUILD QUERY
-    const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach(el => delete queryObj[el]);
-
-    const query = Tour.find(queryObj);
-
-    // const tours = await Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('difficulty')
-    //   .equals('easy');
-
     // EXECUTE QUERY
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .pagination();
+    const tours = await features.query;
 
     // SEND RESPONSE
     res.status(200).json({
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,27 +1,23 @@
-
 const express = require('express');
 const tourController = require('./../controllers/tourController');
 
-
 const router = express.Router();
-
 
 // router.param('id', tourController.checkID);
 
-// create a checkBody middleware
-// check if body contains the name and price property 
-// if not, send back 400 (bad reqest)
-// Add it to the post handler stack
+router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
 
 router
-    .route('/')
-    .get(tourController.getAllTours)
-    .post(tourController.createTour);
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.createTour);
 
 router
-    .route('/:id')
-    .get(tourController.getTour)
-    .patch(tourController.updateTour)
-    .delete(tourController.deleteTour)
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
 
 module.exports = router;
