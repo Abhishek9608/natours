7ea962247cde542df8c4f577af4ff190e40b7fd7
unknown <abhishekkumar9608@gmail.com>
2019-10-01 19:41:25 +0530

    updated schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-const validator = require('validator');
+// const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -9,9 +9,9 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
-      maxlength: [40, 'A tour name must have less or equal then 40 character'],
-      minlength: [10, 'A tour name must have more or equal then 10 character']
-      // validate: [validator.isAlpha, 'Tour name must only contain char']
+      maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+      minlength: [10, 'A tour name must have more or equal then 10 characters']
+      // validate: [validator.isAlpha, 'Tour name must only contain characters']
     },
     slug: String,
     duration: {
@@ -20,21 +20,21 @@ const tourSchema = new mongoose.Schema(
     },
     maxGroupSize: {
       type: Number,
-      required: [true, 'A tour must have a maxGroupSize']
+      required: [true, 'A tour must have a group size']
     },
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
       enum: {
         values: ['easy', 'medium', 'difficult'],
-        message: 'Difficulty is either: easy , medium, difficult'
+        message: 'Difficulty is either: easy, medium, difficult'
       }
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
-      min: [1, 'Rating must be above 1'],
-      max: [5, 'Rating must be below 5']
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0']
     },
     ratingsQuantity: {
       type: Number,
@@ -48,16 +48,16 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       validate: {
         validator: function(val) {
-          // this only points to current docs
+          // this only points to current doc on NEW document creation
           return val < this.price;
         },
-        message: 'Discount price should be below regular price'
+        message: 'Discount price ({VALUE}) should be below regular price'
       }
     },
     summary: {
       type: String,
       trim: true,
-      required: [true, 'tour must have description']
+      required: [true, 'A tour must have a description']
     },
     description: {
       type: String,
@@ -65,7 +65,7 @@ const tourSchema = new mongoose.Schema(
     },
     imageCover: {
       type: String,
-      required: [true, 'a tour must have a cover image']
+      required: [true, 'A tour must have a cover image']
     },
     images: [String],
     createdAt: {
@@ -89,14 +89,14 @@ tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 });
 
-//  DOCUMNET MIDDLEWARE runs before .save() and create
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
 tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
 
 // tourSchema.pre('save', function(next) {
-//   console.log('will save document..');
+//   console.log('Will save document...');
 //   next();
 // });
 
@@ -105,7 +105,8 @@ tourSchema.pre('save', function(next) {
 //   next();
 // });
 
-// Query Middleware
+// QUERY MIDDLEWARE
+// tourSchema.pre('find', function(next) {
 tourSchema.pre(/^find/, function(next) {
   this.find({ secretTour: { $ne: true } });
 
@@ -114,15 +115,15 @@ tourSchema.pre(/^find/, function(next) {
 });
 
 tourSchema.post(/^find/, function(docs, next) {
-  console.log(`Query took ${Date.now() - this.start} milliseconds`);
-  // console.log(docs);
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
   next();
 });
 
 // AGGREGATION MIDDLEWARE
 tourSchema.pre('aggregate', function(next) {
   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
-  console.log(this.pipeline);
+
+  console.log(this.pipeline());
   next();
 });
 
